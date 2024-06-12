bb979191a45bbde78a32b424634f8f1402102ced
kiganyamburu <mburukiganya@gmail.com>
2024-06-12 17:45:54 +0300

    done

---
+++ 11.6 Bmi calculator/scripts.js
@@ -0,0 +1,20 @@
+//Create your function below this line.
+//The first parameter should be the weight and the second should be the height.
+
+let kg = prompt("What is your weight in kg?: ");
+let height = prompt("What is your height in meters?: ");
+
+function bmiCalculator(kg, height) {
+  let bmi = kg / (height * height);
+
+  let answer = alert("your bmi is " + bmi);
+}
+bmiCalculator(kg, height);
+
+/* If my weight is 65Kg and my height is 1.8m, I should be able to call your function like this:
+
+var bmi = bmiCalculator(65, 1.8); 
+
+bmi should equal 20 when it's rounded to the nearest whole number.
+
+*/
