c05bd492dd25613626d34329d8ea5e9c9e1fddc2
kiganyamburu <mburukiganya@gmail.com>
2024-06-14 21:58:12 +0300

    comparatives

--- 11.7 Random Number Generator/script.js
+++ 11.7 Random Number Generator/script.js
@@ -2,5 +2,10 @@ prompt("Enter your name: ");
 prompt("Enter your name: ");
 var love = Math.random() * 100;
 love = Math.floor(love) + 1;
+if (love === 100) {
+  alert("your love score is 100%");
+} else {
+  alert("your love score is " + love + "%");
+}
 
 alert("your love score is " + love);
