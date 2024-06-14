81f94314d111d5840cf5f46e81162aaefa82ec4c
kiganyamburu <mburukiganya@gmail.com>
2024-06-15 01:31:13 +0300

    arrays

---
+++ 11.10 Arrays/main.js
@@ -0,0 +1,10 @@
+// working with Arrays in js
+
+let names = ["jake", "Peter", "john", "jane", "joe", "james", "jim"];
+
+let wname = prompt("What is your name?");
+if (names.includes(wname)) {
+  alert("You are welcome");
+} else {
+  alert("You are not welcome");
+}
--- 11.9 Leap year/main.js
+++ 11.9 Leap year/main.js
@@ -1,18 +1,15 @@
 function leapYearChecker(year) {
   if (year % 4 === 0) {
-    console.log(year + " is  a leap year.");
-  } else {
-    console.log(year + " is not a leap year");
-    if (year % 4 === 0 && year % 100 === 0) {
-      console.log(year + " is a leap year.");
-    } else {
-      console.log(year + " is not a leap year");
-      if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
-        console.log(year + " is a leap year.");
+    if (year % 100 === 0) {
+      if (year % 400 === 0) {
+        return "leap Year";
       } else {
-        console.log(year + " is not a leap year");
+        return "not a leap Year";
       }
+    } else {
+      return "leap Year";
     }
+  } else {
+    return "Not a leap year";
   }
 }
-leapYearChecker(2000);
